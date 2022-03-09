d496cc19dbcbe3f5cc01883f745a4293e88979bc
Quentin Read <quentin.read@usda.gov>
2022-03-09 06:44:42 -0500

    Bugfix quantile drop=false

--- R/emm_quantile.R
+++ R/emm_quantile.R
@@ -8,7 +8,7 @@
 emm_quantile <- function(x, probs = c(0.5, 0.005, 0.995, 0.025, 0.975, 0.05, 0.95, 0.17, 0.83)) {
   if (x@roles$predictors[1] == 'contrast') {
     cc <- combn(1:(nrow(x@misc$orig.grid)), 2)
-    levs <- as.data.frame(t(apply(cc, 2, function(ii) apply(x@misc$orig.grid[ii, ], 1, paste, collapse = ' '))))
+    levs <- as.data.frame(t(apply(cc, 2, function(ii) apply(x@misc$orig.grid[ii, , drop = FALSE], 1, paste, collapse = ' '))))
     names(levs) <- c('group1', 'group2')
   } else {
     levs <- expand.grid(x@levels)
